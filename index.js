33a23eb8271457edeb6fb7bc8defc60375fb1f92
mo <feng_zilong@163.com>
2017-01-26 13:25:29 +0800

    Move babel-plugin-transform-runtime to dependencies

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ module.exports = function( content ) {
 	var defaultLoaders = {
 		html: precompileLoaderPath + '!' + htmlLoaderPath,
 		css: 'style-loader!css-loader',
-		js: 'babel-loader?presets[]=' + require.resolve( 'babel-preset-es2015' ) + '&plugins[]=transform-runtime&comments=false'
+		js: 'babel-loader?presets[]=' + require.resolve( 'babel-preset-es2015' ) + '&plugins[]=' + require.resolve( 'transform-runtime' ) + '&comments=false'
 	};
 	var defaultLang = {
 		template: 'html',
